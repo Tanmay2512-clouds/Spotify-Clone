ceb7718c91996e8b5ce1cd05bd90793757d853ac
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-06 20:03:40 +0530

    feat: add cookie-parser middleware and create server.js file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser")
 
 const app = express(); //server instance created
 app.use(express.json()); //middleware created
+app.use(cookieParser());
 
 module.exports = app;
 
