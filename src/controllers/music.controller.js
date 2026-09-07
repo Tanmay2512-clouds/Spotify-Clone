8e20122a635ea2bf75af41b19939f50d73570c3e
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 19:59:47 +0530

    refactor: improve formatting and structure in createMusic function

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,45 +1,55 @@
-const musicModel = require("../models/music.models")
-const {uploadFile} = require("../services/storage.service")
-const jwt = require("jsonwebtoken")
+const musicModel = require("../models/music.models");
+const { uploadFile } = require("../services/storage.service");
+const jwt = require("jsonwebtoken");
 
-async function createMusic(req,res) {
+async function createMusic(req, res) {
+    const token = req.cookies.token;
 
-    const token = req.cookies.token
-
-    if(!token){
+    if (!token) {
         return res.status(401).json({
-            message:"Unauthorized"
-        })
-    }
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
-        if(decoded.role!=="artist"){
-            return res.status(403).json({message:"You don't have access to create music"})
-        }
-    }catch(err){
-        return res.status(401).json({message:"Unauthorized"})
+            message: "Unauthorized"
+        });
     }
 
-    const {title} = req.body;
-    const file = req.file;  
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const result = await uploadFile(file.buffer.toString('base64'))
-
-    const music = await musicModel.create({
-        uri:result.uri,
-        title,
-        artist:decode.id
-
-    })
-
-    res.status(201).json({
-        message:"music created successfully",
-        music:{
-            id:music._id,
-            uri:music.uri,
-            title:music.title,
-            artist:music.artist
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "You don't have access to create music"
+            });
         }
-    })
+
+        const { title } = req.body;
+        const file = req.file;
+
+        const result = await uploadFile(
+            file.buffer.toString("base64")
+        );
+
+        const music = await musicModel.create({
+            uri: result.uri,
+            title,
+            artist: decoded.id
+        });
+
+        res.status(201).json({
+            message: "music created successfully",
+            music: {
+                id: music._id,
+                uri: music.uri,
+                title: music.title,
+                artist: music.artist
+            }
+        });
+
+    } catch (err) {
+        console.error(err)
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
 }
-module.exports = {createMusic}
+
+module.exports = { createMusic };
+
