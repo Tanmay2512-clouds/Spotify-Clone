727a14e97d4f1d791dfc2ee20f01a0844b12d5c7
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 17:29:37 +0530

    feat: set up music routes and controllers for music-related functionality

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
 const authRoutes = require("./routes/auth.routes")
+const musicRoutes = require("./routes/music.routes")
 
 const app = express(); //server instance created
 app.use(express.json()); //middleware created
 app.use(cookieParser());
 app.use('/api/auth',authRoutes)
+app.use('/api/music',musicRoutes)
 
 module.exports = app;
 
