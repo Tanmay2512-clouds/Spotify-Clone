1f176dc49c7af7a06e15b10426f498920513119c
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 17:19:07 +0530

    refactor: improve formatting and consistency in loginUser function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -48,46 +48,51 @@ async function registerUser(req, res) {
     });
 }
 
-async function loginUser(req,res){
-    const {username,email,password} = req.body;
+async function loginUser(req, res) {
+    const { username, email, password } = req.body;
 
     const user = await userModel.findOne({
-        $or:[
-            {username},
-            {email}
+        $or: [
+            { username },
+            { email }
         ]
-    })
+    });
 
-    if(!user){
+    if (!user) {
         return res.status(401).json({
-            message:"Invalid Credentials"
-        })
+            message: "Invalid Credentials"
+        });
     }
 
-    const isPasswordValid = await bcrypt.compare(password,user.password)
+    const isPasswordValid = await bcrypt.compare(
+        password,
+        user.password
+    );
 
-    if(!isPasswordValid){
+    if (!isPasswordValid) {
         return res.status(401).json({
-            message:"Ivalid Credentials"
-        })
+            message: "Invalid Credentials"
+        });
     }
 
-    const token = jwt.sign({
-        id:user._id,
-        role:user.role
-    },process.env.JWT_SECRET)
+    const token = jwt.sign(
+        {
+            id: user._id,
+            role: user.role
+        },
+        process.env.JWT_SECRET
+    );
 
-    res.cookie("token",token)
+    res.cookie("token", token);
 
     res.status(200).json({
-        message:"User Logged in Successfully",
-        user:{
-            id:user._id,
-            username:user.username,
-            email:user.email,
-            role:user.role,
+        message: "User Logged in Successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
         }
-    })
+    });
 }
-
 module.exports = { registerUser , loginUser };
