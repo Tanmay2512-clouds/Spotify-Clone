ea565c584d51ac3222b1f5228da1880b95a6b3a0
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 19:35:07 +0530

    feat: integrate multer for handling music file uploads in routes

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const musicController = require("../controllers/music.controller")
-
+const multer = require("multer")
 const router = express.Router();
 
-router.post("/upload",musicController.createMusic)
+const upload = multer({
+    storage:multer.memoryStorage()
+})
+
+router.post("/upload",upload.single("music"),musicController.createMusic)
 
 
-module.exports = router;    
+module.exports = router;        
