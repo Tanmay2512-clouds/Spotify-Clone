2b4fc982570692d023b3c6f1baba1b9a778da9aa
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 18:15:26 +0530

    feat: add music schema for music-related data modeling

---
+++ src/models/music.models.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const musicSchema = new mongoose.Schema({
+    uri:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    artist:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    }
+})
