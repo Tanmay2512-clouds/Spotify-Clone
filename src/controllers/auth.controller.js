0bbfd3b21096def0bd166545c0b45ad43cf48f2d
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 14:06:27 +0530

    feat: enhance registerUser function with user existence check and JWT token generation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,32 @@
 const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken')
 
 async function registerUser(req,res){
     const{username,email,password,role="user"} = req.body
 
     const isUserAlreadyExist = await userModel.findOne({
-        
+        $or:[
+            {username},
+            {email}
+        ]
+
+    })
+
+    if(isUserAlreadyExist){
+        return res.status(409).json({
+            message:"User Already Exists"
+        })
+    }
+
+    const user = await userModel.create({
+        username,
+        email,
+        password,
+        role,
+    })
+
+    const token = jwt.sign({
+        id:user._id,
+        role:user.role
     })
 }
