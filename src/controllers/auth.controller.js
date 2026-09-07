71fbce87acc0642805d6001be0bf69d62f64fc4f
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 13:43:20 +0530

    feat: implement user existence check in registerUser function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 const userModel = require('../models/user.model');
 
 async function registerUser(req,res){
-    
+    const{username,email,password,role="user"} = req.body
+
+    const isUserAlreadyExist = await userModel.findOne({
+        
+    })
 }
