dc5fe03ab11107a90e430f55a330f49243dd4a7f
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 18:41:01 +0530

    feat: implement createMusic function with JWT authentication in music controller

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,5 +1,19 @@
 const musicModel = require("../models/music.models")
+const jwt = require("jsonwebtoken")
 
-async function createModel(req,res) {
+async function createMusic(req,res) {
+
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized"
+        })
+    }
+    try{
+        jwt.verify(token,process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({message:"Unauthorized"})
+    }
     
 }
