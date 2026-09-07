3b8c493bbd92b6f977ef3612504f4044f3cc3c82
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-07 19:03:56 +0530

    feat: update uploadFile function to specify music folder for file uploads

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -8,6 +8,6 @@ async function uploadFile(file){
     const result = await ImageKitClient.files.upload({
         file,
         fileName:"music_"+Date.now(),
-        folder:"yt-complete-backend"
+        folder:"yt-complete-backend/music"
     })
 }
