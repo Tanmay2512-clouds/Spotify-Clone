e9d7c24a0244547e62392e3a837e8e0663553d11
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-08 13:01:54 +0530

    feat: implement createAlbum function with authorization and album creation logic

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -77,7 +77,40 @@ async function createMusic(req, res) {
 }
 
 async function createAlbum(req,res){
-    
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"})
+    }
+    try{
+        
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        if(!decoded.role!=="artist"){
+            return res.status(403).json({message:"You don't have access to create Album"})
+        }
+
+        const {title,musicIds}= req.body
+
+        const album = await albumModel.create({
+            title,
+            artist:decoded.id,
+            musics:musicIds
+        })
+
+        res.status(201).json({
+            message:"Album Created Successfully",
+            album:{
+                id:album._id,
+                title:album.title,
+                artist:album.artist,
+                music:album.musics,
+            }
+        })
+
+    }catch(err){
+        console.error(err)
+        return res.status(401).json({message:"Unauthorized"})
+    }
 }
 
 
